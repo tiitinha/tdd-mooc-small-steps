6d975b9ffe38c6c9cc3ad5010d2f117fdd7a7b2d
afroseppo <kobo-kobe@hotmail.com>
2023-01-14 16:43:57 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -70,14 +70,14 @@ function createApp(database) {
 
   function calculateReduction(date) {
     let reduction = 0;
-    if (date && isMonday(date) && !isHoliday(date)) {
+    if (date && isMonday(Temporal.PlainDate.from({ year: date.getFullYear(), month: date.getMonth(), day: date.getDate() })) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
   }
 
   function isMonday(date) {
-    return Temporal.PlainDate.from({ year: date.getFullYear(), month: date.getMonth(), day: date.getDate() }).dayOfWeek === 1;
+    return date.dayOfWeek === 1;
   }
 
   function isHoliday(date) {
